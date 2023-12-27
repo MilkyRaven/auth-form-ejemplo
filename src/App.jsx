d5a4a1ed08e9330f26fe3ffcfe7e68b80e7befa5
Milky Raven <heartonllamas@gmail.com>
2023-12-27 19:53:19 +0100

    fix folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useContext } from 'react'
 import './App.css'
 //import ReactHookComponent from './assets/components/ReactHookComponent'
-import TraditionalFormComponent from './assets/components/TraditionalFormComponent'
-import { AuthContext } from './assets/context/AuthContext'
+import TraditionalFormComponent from './components/TraditionalFormComponent'
+import { AuthContext } from './context/AuthContext'
 import {Toaster} from 'react-hot-toast';
-import LogoutButton from './assets/components/LogoutButton'
+import LogoutButton from './components/LogoutButton'
 function App() {
   //usamos a nivel app el currentUser para determinar qué se muestra y qué no en la app
   //esto será mejorado con las rutas privadas/protegidas
@@ -15,7 +15,7 @@ console.log(currentUser);
     {/* container que permite mostrar notificaciones de ciertos eventos */}
         <Toaster/>
 {!currentUser && <TraditionalFormComponent/>}
-  {/* por ahora estos datos no persisten */}
+  {/* por ahora estos datos no */}
 {currentUser?.email && <div><p> Usuario con email: {currentUser.email} y contraseña {currentUser.password}</p></div>}
       <LogoutButton/>
     </>
